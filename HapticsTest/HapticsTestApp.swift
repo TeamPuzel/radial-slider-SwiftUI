43b5200b8a75032a4fed76b90a28bddaf00a62e8
TeamPuzel <puffed-01.blips@icloud.com>
2022-09-11 23:15:50 +0200

    Initial Commit

---
+++ HapticsTest/HapticsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsTestApp.swift
+//  HapticsTest
+//
+//  Created by Wojciech David Wargocki on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct HapticsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
